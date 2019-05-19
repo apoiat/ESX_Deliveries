b470e94cd0e469d53b6b2b859a508dde08445826
Ap. Iatridis <apo.iatridis@gmail.com>
2019-05-20 00:43:40 +0300

    Update main.lua

--- Esx_Deliveries_Server/main.lua
+++ Esx_Deliveries_Server/main.lua
@@ -1,5 +1,4 @@
 ESX = nil
---local inspect = require('inspect.lua')
 
 TriggerEvent('esx:getSharedObject', function(obj)
     ESX = obj
@@ -33,43 +32,11 @@ end)
 
 RegisterServerEvent('esx_deliveries:AddBankMoney')
 AddEventHandler('esx_deliveries:AddBankMoney', function(amount)
-
-     --print("FightClub :: Trying to request $" .. amount .. " from player :: " .. pID.Name)
-  
+ 
     local xPlayer = ESX.GetPlayerFromId(source)
 
     xPlayer.addAccountMoney("bank", amount)
-    --[[
-    if player.getMoney() >= amount then
-      TriggerClientEvent("FightClub:BetAccepted", pID.Handle);
-      player.removeMoney(amount);
-    else
-      TriggerClientEvent('esx:showNotification', pID.Handle, "You don't have enough ~r~money~w~.");
-    end
-    --]]
+
   
 end)
 
-
-function dump(o)
-   if type(o) == 'table' then
-      local s = '{ '
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. dump(v) .. ','
-      end
-      return s .. '} '
-   else
-      return tostring(o)
-   end
-end
-
---[[
-RegisterServerEvent('MailDelivery:VanRented')
-AddEventHandler('MailDelivery:VanRented', function(amount)
-    local _source = source
-
-    local player = ESX.GetPlayerFromId(_source)
-    player.removeMoney(tonumber(amount))
-end)
---]]
